b523846ef114335df969e0723fe41867fc84e255
Martin Peeters <martin.peeters@affinitic.be>
2014-08-13 10:54:19 +0200

    Pep8 fix

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-import os
 
 version = '0.1'
 
@@ -14,36 +13,36 @@ long_description = (
     open('CHANGES.rst').read()
     + '\n')
 
-setup(name='dexterity.localrolesfield',
-      version=version,
-      description="z3c.form local role field for dexterity",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='dexterity.localrolesfield',
+    version=version,
+    description="z3c.form local role field for dexterity",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='',
-      author='IMIO',
-      author_email='support@imio.be',
-      url='https://github.com/collective/',
-      license='gpl',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['dexterity', ],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      extras_require={'test': ['plone.app.testing']},
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+    ],
+    keywords='',
+    author='IMIO',
+    author_email='support@imio.be',
+    url='https://github.com/collective/',
+    license='gpl',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['dexterity', ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+    extras_require={'test': ['plone.app.testing']},
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+)
